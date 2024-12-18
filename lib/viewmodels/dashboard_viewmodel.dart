03f5b56917c321b7ae6261b5cd98472e349d3ba8
Jean WITTMANN <wittmann.jean@gmail.com>
2024-12-18 23:43:12 +0100

    [main] : add all characters data

--- lib/viewmodels/dashboard_viewmodel.dart
+++ lib/viewmodels/dashboard_viewmodel.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/foundation.dart';
+import '../models/gender.dart';
 import '../services/api_service.dart';
 import '../models/result_state.dart';
 import '../models/results.dart';
@@ -41,5 +42,88 @@ class DashboardViewModel extends ChangeNotifier {
     _state = ResultState.success(fetchedCharacters);
     _isFetching = false;
     notifyListeners();
+  }
+
+  Future<void> fetchAllCharactersNoPagination() async {
+    if (_isFetching) return;
+    _isFetching = true;
+
+    if (!_hasMore) return;
+    notifyListeners();
+
+    List<Results> fetchedCharacters = [];
+
+    while (_hasMore) {
+      try {
+        final result =
+        await _apiService.fetchAllCharacters(page: _currentPage);
+        final newCharacters = result.results;
+        fetchedCharacters.addAll(newCharacters);
+        _hasMore = result.info.next != null;
+        _currentPage++;
+      } catch (e) {
+        _state = ResultState.error(e.toString());
+        notifyListeners();
+        return;
+      }
+    }
+
+    _state = ResultState.success(fetchedCharacters);
+    _isFetching = false;
+    notifyListeners();
+  }
+
+  int getNumberOfHuman() {
+    if(state.hasData) {
+      return state.data?.where((character) => character.species == "Human").length ?? 0;
+    } else {
+      return 0;
+    }
+  }
+
+  int getNumberOfAliveCharacter() {
+    if(state.hasData) {
+      return state.data?.where((character) => character.status == "Alive").length ?? 0;
+    } else {
+      return 0;
+    }
+  }
+
+  int getNumberOfDeadCharacter() {
+    if(state.hasData) {
+      return state.data?.where((character) => character.status == "Dead").length ?? 0;
+    } else {
+      return 0;
+    }
+  }
+
+  int getNumberOfUnknownCharacter() {
+    if(state.hasData) {
+      return state.data?.where((character) => character.status == "unknown").length ?? 0;
+    } else {
+      return 0;
+    }
+  }
+
+  int getNumberOfGenderCharacter(Gender gender) {
+    if(state.hasData) {
+      return state.data?.where((character) => character.gender == gender.name).length ?? 0;
+    } else {
+      return 0;
+    }
+  }
+
+  List<String> getAllSpecies() {
+    List<String> spicesType = [];
+    if(state.hasData) {
+      state.data?.forEach((value) {
+        if(!spicesType.contains(value.species)){
+          spicesType.add(value.species);
+        }
+      });
+      return spicesType;
+    } else {
+      return [];
+    }
   }
 }
--- lib/views/dashboard_data_page.dart
+++ lib/views/dashboard_data_page.dart
@@ -42,7 +42,7 @@ class _DashboardDataPageState extends State<DashboardDataPage> {
                         if (!_useLoadedData) {
                           context
                               .read<DashboardViewModel>()
-                              .fetchAllCharactersBySpecies();
+                              .fetchAllCharactersNoPagination();
                         }
                       });
                     }),
@@ -181,29 +181,130 @@ class _DashboardDataPageState extends State<DashboardDataPage> {
                           )
                         : Consumer<DashboardViewModel>(
                             builder: (context, viewModel, child) {
+                              final speciesList = viewModel.getAllSpecies();
+
                               if (viewModel.state.isLoading) {
                                 return Center(
                                     child: CircularProgressIndicator());
                               }
-                              return Center(
-                                  child: Stack(
+                              return Stack(
                                 children: [
                                   Column(
-                                    crossAxisAlignment:
-                                        CrossAxisAlignment.start,
-                                    children: [
-                                      Padding(
-                                        padding: const EdgeInsets.only(
-                                            top: 16.0, left: 10.0),
-                                        child: Text(
-                                          ' - Number of Human loaded characters: ${viewModel.state.data!.length}',
-                                          style: TextStyle(fontSize: 24),
+                                      crossAxisAlignment:
+                                      CrossAxisAlignment.start,
+                                      children: [
+                                        Padding(
+                                          padding: const EdgeInsets.only(
+                                              top: 16.0, left: 10.0),
+                                          child: Text(
+                                            '- Number of loaded characters: ${viewModel.state.data!.length}',
+                                            style: TextStyle(fontSize: 24),
+                                          ),
                                         ),
-                                      ),
-                                    ],
-                                  ),
+                                        Padding(
+                                          padding:
+                                          const EdgeInsets.only(left: 10.0),
+                                          child: Text(
+                                            '- Number of Human: ${viewModel.getNumberOfHuman()}',
+                                            style: TextStyle(fontSize: 24),
+                                          ),
+                                        ),
+                                        Padding(
+                                          padding:
+                                          const EdgeInsets.only(left: 10.0),
+                                          child: Text(
+                                            '- Number of Alive Character: ${viewModel.getNumberOfAliveCharacter()}',
+                                            style: TextStyle(fontSize: 24),
+                                          ),
+                                        ),
+                                        Padding(
+                                          padding:
+                                          const EdgeInsets.only(left: 10.0),
+                                          child: Text(
+                                            '- Number of Dead Character: ${viewModel.getNumberOfDeadCharacter()}',
+                                            style: TextStyle(fontSize: 24),
+                                          ),
+                                        ),
+                                        Padding(
+                                          padding:
+                                          const EdgeInsets.only(left: 10.0),
+                                          child: Text(
+                                            '- Number of Unknown Character: ${viewModel.getNumberOfUnknownCharacter()}',
+                                            style: TextStyle(fontSize: 24),
+                                          ),
+                                        ),
+                                        Padding(
+                                          padding:
+                                          const EdgeInsets.only(left: 10.0),
+                                          child: Text(
+                                            '- Number of Male Character: ${viewModel.getNumberOfGenderCharacter(Gender.male)}',
+                                            style: TextStyle(fontSize: 24),
+                                          ),
+                                        ),
+                                        Padding(
+                                          padding:
+                                          const EdgeInsets.only(left: 10.0),
+                                          child: Text(
+                                            '- Number of Female Character: ${viewModel.getNumberOfGenderCharacter(Gender.female)}',
+                                            style: TextStyle(fontSize: 24),
+                                          ),
+                                        ),
+                                        Padding(
+                                          padding:
+                                          const EdgeInsets.only(left: 10.0),
+                                          child: Text(
+                                            '- Number of Genderless Character: ${viewModel.getNumberOfGenderCharacter(Gender.genderless)}',
+                                            style: TextStyle(fontSize: 24),
+                                          ),
+                                        ),
+                                        Padding(
+                                          padding:
+                                          const EdgeInsets.only(left: 10.0),
+                                          child: Text(
+                                            '- Number of Unknown Gender Character: ${viewModel.getNumberOfGenderCharacter(Gender.unknown)}',
+                                            style: TextStyle(fontSize: 24),
+                                          ),
+                                        ),
+                                        Padding(
+                                          padding:
+                                          const EdgeInsets.only(left: 10.0),
+                                          child: Text(
+                                            '- Number of spices: ${viewModel.getAllSpecies().length}',
+                                            style: TextStyle(fontSize: 24),
+                                          ),
+                                        ),
+                                        Padding(
+                                          padding: const EdgeInsets.only(
+                                              top: 10.0, left: 10.0),
+                                          child: Text(
+                                            'Type of spices: ',
+                                            style: TextStyle(fontSize: 24),
+                                          ),
+                                        ),
+                                        Padding(
+                                          padding:
+                                          const EdgeInsets.only(left: 10.0),
+                                          child: ListView.builder(
+                                              shrinkWrap: true,
+                                              physics:
+                                              NeverScrollableScrollPhysics(),
+                                              itemCount: speciesList.length,
+                                              itemBuilder: (context, index) {
+                                                return Padding(
+                                                  padding:
+                                                  const EdgeInsets.only(
+                                                      left: 10.0),
+                                                  child: Text(
+                                                    speciesList[index],
+                                                    style:
+                                                    TextStyle(fontSize: 24),
+                                                  ),
+                                                );
+                                              }),
+                                        ),
+                                      ]),
                                 ],
-                              ));
+                              );
                             },
                           ),
                   ],
